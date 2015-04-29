56adad08ce449c4222a5b63df851b2f4198f1f9c
m-kozelchuk <marina.kozelchuk@gmail.com>
2015-04-29 14:20:22 +0300

    done

---
+++ js/responsive.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($) {
+
+	$(".js-menu-mobile").click(function(){
+		$(".menu").addClass("active");
+		$(".menu-button").addClass("active");
+		$(".js-menu-mobile>p").css("display", "none");
+		$(".js-menu-close").css("display", "block");
+		return false;
+	});
+	$(".js-menu-close").click(function(){
+		$(".menu").removeClass("active");
+		$(".menu-button").removeClass("active");
+		$(".js-menu-mobile>p").css("display", "block");
+		$(".js-menu-close").css("display", "none");
+		return false;
+	});
+
+});
